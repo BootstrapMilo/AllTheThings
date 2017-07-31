100e222775f2edbe1ac41e7fd3e3c34655c28367
BoostrapMilo <danielmilofarkner@gmail.com>
2017-07-31 22:53:55 +0200

    Exploit changed to payloads

---
+++ AllTheThings/Library42/IPayload.cs
@@ -0,0 +1,11 @@
+﻿namespace AllTheThings.Library42
+{
+    public interface IPayload
+    {
+        string payloadElement { get; set; }
+        string payloadEvent { get; set; }
+        string payloadFunction { get; set; }
+        string target { get; set; }
+        string tag { get; set; }
+    }
+}
